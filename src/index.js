37b8897ff0dfd47ba163d31d8b6056fe9fe7ddfd
klemen <klemen.komel@gmail.com>
2019-06-06 00:17:37 +0200

    added logger midleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { Provider} from 'react-redux';
 import { createStore } from 'redux';
+import { createLogger } from 'redux-logger';
 
 import App from './containers/App';
 import { searchRobots } from './reducers.js';
@@ -11,6 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'tachyons';
 
+const logger = createLogger();
 const store = createStore(searchRobots);
 
 ReactDOM.render(
